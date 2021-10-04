cdf41f8fcc765ddfaddcc1a580bb7a8f7d6add54
Jose Daniel Cabrera Calderon <jose.cabrera.2021@upb.edu.co>
2021-10-04 17:22:16 -0500

    Agregar archivos de proyecto.

---
+++ Blackjack Zero/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Zero
+{
+    public class Card
+    {
+        public string suit { get; set; }
+        public string symbol { get; set; }
+        public int score { get; set; }
+        public string color { get; set; }
+
+        public Card(string suit, string symbol, int score, string color)
+        {
+            this.suit = suit;
+            this.symbol = symbol;
+            this.score = score;
+            this.color = color;
+        }
+    }
+}
