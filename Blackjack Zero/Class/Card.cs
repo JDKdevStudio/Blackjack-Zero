37a951d932ddc72804e0de703921b546ccdf0c26
Jose Daniel Cabrera Calderon <jose.cabrera.2021@upb.edu.co>
2021-10-07 17:57:07 -0500

    Color, Dealer, Card classes created

---
+++ Blackjack Zero/Class/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Zero
+{
+    public class Card
+    {
+        public char Suit { get; set; }
+        public string Symbol { get; set; }
+        public int Score { get; set; }
+
+        public Color Cardcolor { get; set; }
+
+        public Card(char suit, string symbol)
+        {
+            Suit = suit;
+            Symbol = symbol;
+            if (suit.Equals('♥') || suit == '♦')
+            {
+                Cardcolor = Color.Red;
+            }
+            else
+            {
+                Cardcolor = Color.Black;
+
+            }
+            if (symbol == "A")
+                Score = 11;
+            else if (symbol == "J" || symbol == "Q" || symbol == "K")
+                Score = 10;
+            else
+                Score = Convert.ToInt32(symbol);
+        }
+    }
+}
---
+++ Blackjack Zero/Class/Dealer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack_Zero.Class
+{
+    class Dealer
+    {
+        private List<Card> deck;
+        private List<Card> hand;
+
+        public void Generate()
+        {
+            deck = new List<Card>();
+            char[] suits = { '♦', '♥', '♠', '♣' };
+            string[] symbols = {"A","2","3","4","5","6",
+                                "5","6","7","8","9","10","J","K","Q"};
+            foreach (char s in suits)
+            {
+                foreach (string sy in symbols)
+                {
+                    deck.Add(new Card(s, sy));
+                }
+            }
+            Randomize();
+        }
+        public List<Card> GetDeck()
+        {
+            return deck;
+        }
+        private void Randomize()
+        {
+            Random rnd = new Random();
+            this.deck = this.deck.OrderBy(x => rnd.Next()).ToList();
+        }
+        public void Deal()
+        {
+            Card c = this.deck.First();
+            this.deck.RemoveAt(this.deck.Count - 1);
+            hand.Add(c);
+        }
+    }
+}
