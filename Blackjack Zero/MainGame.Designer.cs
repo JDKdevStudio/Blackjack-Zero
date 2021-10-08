72aecb69a19f16e9168650940af537bef89aa57c
Jose Daniel Cabrera Calderon <jose.cabrera.2021@upb.edu.co>
2021-10-07 22:40:22 -0500

    Diseño inicial terminado

---
+++ Blackjack Zero/MainGame.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Blackjack_Zero
+{
+    partial class MainGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainGame));
+            this.ProfileImage = new System.Windows.Forms.PictureBox();
+            this.Close = new System.Windows.Forms.PictureBox();
+            this.UserPanel = new System.Windows.Forms.PictureBox();
+            this.Cover = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfileImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UserPanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProfileImage
+            // 
+            this.ProfileImage.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ProfileImage.Image = global::Blackjack_Zero.Properties.Resources.man;
+            this.ProfileImage.Location = new System.Drawing.Point(18, 12);
+            this.ProfileImage.Name = "ProfileImage";
+            this.ProfileImage.Size = new System.Drawing.Size(100, 100);
+            this.ProfileImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ProfileImage.TabIndex = 3;
+            this.ProfileImage.TabStop = false;
+            // 
+            // Close
+            // 
+            this.Close.Image = global::Blackjack_Zero.Properties.Resources.Close;
+            this.Close.Location = new System.Drawing.Point(874, 2);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(24, 24);
+            this.Close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Close.TabIndex = 2;
+            this.Close.TabStop = false;
+            // 
+            // UserPanel
+            // 
+            this.UserPanel.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.UserPanel.Location = new System.Drawing.Point(-5, -4);
+            this.UserPanel.Name = "UserPanel";
+            this.UserPanel.Size = new System.Drawing.Size(143, 504);
+            this.UserPanel.TabIndex = 1;
+            this.UserPanel.TabStop = false;
+            // 
+            // Cover
+            // 
+            this.Cover.Image = global::Blackjack_Zero.Properties.Resources.BlackJackCover;
+            this.Cover.Location = new System.Drawing.Point(195, 77);
+            this.Cover.Name = "Cover";
+            this.Cover.Size = new System.Drawing.Size(653, 336);
+            this.Cover.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cover.TabIndex = 0;
+            this.Cover.TabStop = false;
+            // 
+            // MainGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.Controls.Add(this.ProfileImage);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.UserPanel);
+            this.Controls.Add(this.Cover);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BlackJack Zero";
+            ((System.ComponentModel.ISupportInitialize)(this.ProfileImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UserPanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cover)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Cover;
+        private System.Windows.Forms.PictureBox UserPanel;
+        private System.Windows.Forms.PictureBox Close;
+        private System.Windows.Forms.PictureBox ProfileImage;
+    }
+}
